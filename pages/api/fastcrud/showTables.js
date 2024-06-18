ba7f3596f2aefd934b5c1696aec23baffcb62a39
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2024-06-17 18:11:29 -0600

    nueva act

--- pages/api/fastcrud/showTables.js
+++ pages/api/fastcrud/showTables.js
@@ -15,7 +15,7 @@ export default async function (req, res) {
         port: DBPORT
     });
     const connection = await pool.getConnection();
-    const sqlQuery = "show tables";
+    const sqlQuery = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'";
     const [rows, fields] = await connection.execute(sqlQuery);
     const tablas = rows.map(table => table = table[`Tables_in_${DBNAME}`]);
     res.status(200).json({
